fcbc0041af75d9be00ed1dab7fdbb77d3ee25e28
R I Rayhan <islamraihanofficial@gmail.com>
2026-07-11 16:04:17 +0600

    feat(rental): implement rental request routing with tenant endpoints

---
+++ src/modules/rental/rental.route.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { rentalController } from "./rental.controller";
+import { auth } from "../../middlewares/auth";
+import { UserRole } from "../../../generated/prisma/enums";
+
+const router = Router();
+// Tenant routes
+router.post("/", auth(UserRole.TENANT), rentalController.createRentalRequest);
+router.get(
+  "/:id",
+  auth(UserRole.TENANT),
+  rentalController.getTenantRequestsDetail,
+);
+router.get(
+  "/",
+  auth(UserRole.TENANT),
+  rentalController.getTenantRentalRequests,
+);
+
+// Landlord Routes
+// router.get(
+//   "/landlord/requests/all",
+//   auth(UserRole.LANDLORD),
+//   rentalController.getLandlordAllRequests,
+// );
+// router.get(
+//   "/landlord/requests/:id",
+//   auth(UserRole.LANDLORD),
+//   rentalController.getLandlordRequestsDetail,
+// );
+// router.patch(
+//   "/landlord/requests/:id",
+//   auth(UserRole.LANDLORD),
+//   rentalController.updateLandlordRequestStatus,
+// );
+
+export const rentalRouter = router;
